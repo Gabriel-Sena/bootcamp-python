611482b2d7e04cd0a2ae006ddd1648a63202fbf3
Gabriel-Sena <gsdcdj@gmail.com>
2024-04-25 13:53:26 -0300

    Variáveis e constantes

---
+++ cod/variaveis_constantes.py
@@ -0,0 +1,17 @@
+# declarando variáveis
+nome = "Gabriel"
+idade = 20
+print(nome, idade)
+
+
+# alterando variáveis
+nome = "Rosane"
+idade = 21
+print(nome, idade)
+
+# nome sugestivo
+limite_saque_diario = 1000  
+
+# constante
+ESTADOS_BRASILIROS = ["SP", "RJ", "BA"]
+print(ESTADOS_BRASILIROS)
