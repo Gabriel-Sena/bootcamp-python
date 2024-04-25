9513be30b4f490daa4976cff366dd1e481f9ac29
Gabriel-Sena <gsdcdj@gmail.com>
2024-04-25 13:53:48 -0300

    Conversão

---
+++ cod/convertendo_tipos.py
@@ -0,0 +1,13 @@
+preco = 10.50
+preco = int(preco)
+print(preco)
+
+print(float("10.45"))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
